ad5d3b4e4c231506852c33630b215e9fb39db10d
Pulipuli Chen <pulipuli.chen@gmail.com>
2018-05-26 15:01:00 +0800

    0526 WebIntent開始

--- www/script.js
+++ www/script.js
@@ -1,18 +1,30 @@
 ready = function () {
     try {
-    window.plugins.webintent.startActivity({
-        action: window.plugins.webintent.ACTION_VIEW,
-        url: theFile.toURL(),
-        type: 'application/vnd.android.package-archive'
-    },
-            function () {},
-            function () {
-                alert('Failed to open URL via Android Intent.');
-                console.log("Failed to open URL via Android Intent. URL: " + theFile.fullPath)
-            }
-    );
-    }
-    catch (e) {
+        /*
+         window.plugins.webintent.startActivity({
+         action: window.plugins.webintent.ACTION_VIEW,
+         url: theFile.toURL(),
+         type: 'application/vnd.android.package-archive'
+         },
+         function () {},
+         function () {
+         alert('Failed to open URL via Android Intent.');
+         console.log("Failed to open URL via Android Intent. URL: " + theFile.fullPath)
+         }
+         );
+         */
+        window.plugins.webintent.startActivity({
+            action: window.plugins.webintent.ACTION_VIEW,
+            url: 'geo:0,0?q='
+        },
+                function () {
+                    alert("work")
+                },
+                function () {
+                    alert('Failed to open URL via Android Intent')
+                };
+                );
+    } catch (e) {
         alert("fail: " + e);
     }
 };
