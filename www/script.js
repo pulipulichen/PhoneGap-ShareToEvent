b8d7d138af513db55c08c79750c790724a20402e
Pulipuli Chen <pulipuli.chen@gmail.com>
2018-05-26 22:43:32 +0800

    0526 WebIntent 可以安裝 intent-filter shortcut呢？

--- www/script.js
+++ www/script.js
@@ -76,34 +76,57 @@ ready = function () {
          // `hasExtra` check failed
          });
          */
-        window.plugins.Shortcuts.supportsDynamic(function(supported) { 
-    if (supported)
-        window.alert('Dynamic shortcuts are supported');
-    else
-        window.alert('Dynamic shortcuts are NOT supported');
-}, function(error) {
-    window.alert('Error: ' + error);
-})
         
-        window.plugins.intent.setNewIntentHandler(function (intent) {
-            //alert("setNewIntentHandler:" + JSON.stringify(intent));
-            document.wrtie(JSON.stringify(intent));
-        });
+        var Permission = window.plugins.Permission
+ 
+        var permission = 'com.android.launcher.permission.INSTALL_SHORTCUT';
 
-        // 這好像是預設值
-        //window.plugins.intent.getCordovaIntent(function (intent) {
-        //    alert("getCordovaIntent:" + JSON.stringify(intent));
-        //});
-        
-        window.plugins.Shortcut.CreateShortcut("Text to show 1");
-        window.plugins.Shortcut.CreateShortcut({
-            text: "Text to show 2",
-            extraSubject: 'YOUR_DATA'
-        }, function () {
-            alert("ok");
-        }, function () {
-            alert("error");
-        });
+        Permission.has(permission, function (results) {
+            if (!results[permission]) {
+                Permission.request(permission, function (results) {
+                    if (results[permission]) {
+                        // permission is granted
+                        _run();
+                    }
+                }, function (e) {
+                    alert("Error: " + e);
+                })
+            }
+        },  function (e) {
+                    alert("Error: " + e);
+                })
+                
+        var _run = function () {
+            window.plugins.Shortcuts.supportsDynamic(function (supported) {
+                if (supported)
+                    window.alert('Dynamic shortcuts are supported');
+                else
+                    window.alert('Dynamic shortcuts are NOT supported');
+            }, function (error) {
+                window.alert('Error: ' + error);
+            })
+
+            window.plugins.intent.setNewIntentHandler(function (intent) {
+                //alert("setNewIntentHandler:" + JSON.stringify(intent));
+                document.wrtie(JSON.stringify(intent));
+            });
+
+            // 這好像是預設值
+            //window.plugins.intent.getCordovaIntent(function (intent) {
+            //    alert("getCordovaIntent:" + JSON.stringify(intent));
+            //});
+
+            window.plugins.Shortcut.CreateShortcut("Text to show 1");
+            window.plugins.Shortcut.CreateShortcut({
+                text: "Text to show 2",
+                extraSubject: 'YOUR_DATA'
+            }, function () {
+                alert("ok");
+            }, function () {
+                alert("error");
+            });
+        };
+            
     } catch (e) {
         alert("fail: " + e);
     }
