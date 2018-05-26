9795c1e58f7db24705c7c12280a7a2e7f0086a20
Pulipuli Chen <pulipuli.chen@gmail.com>
2018-05-26 15:23:59 +0800

    0526 WebIntent 不懂

--- www/script.js
+++ www/script.js
@@ -14,6 +14,7 @@ ready = function () {
          );
          */
         alert(1);
+        /*
         window.plugins.webintent.startActivity({
             action: window.plugins.webintent.ACTION_VIEW,
             url: 'geo:0,0?q='
@@ -25,6 +26,19 @@ ready = function () {
                     alert('Failed to open URL via Android Intent')
                 }
         );
+        */
+       window.plugins.webintent.sendBroadcast({
+            action: 'com.dummybroadcast.action.triggerthing',
+            extras: {option: true}
+        }, function () {
+        }, function () {
+        });
+        
+        window.plugins.webintent.onNewIntent(function (uri) {
+            if (uri !== '') {
+                alert(uri);
+            }
+        });
     } catch (e) {
         alert("fail: " + e);
     }
