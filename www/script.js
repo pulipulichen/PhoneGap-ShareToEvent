8e944b7025e4cd342c6e2d7faeab13c0cb2c9745
Pulipuli Chen <pulipuli.chen@gmail.com>
2018-05-26 15:25:25 +0800

    0526 WebIntent 不懂

--- www/script.js
+++ www/script.js
@@ -36,9 +36,24 @@ ready = function () {
         
         window.plugins.webintent.onNewIntent(function (uri) {
             if (uri !== '') {
-                alert(uri);
+                alert("onNewIntent: " + uri);
             }
         });
+        
+        window.plugins.webintent.getUri(function (uri) {
+            if (uri !== '') {
+                // `uri` is the uri the intent was launched with.
+                //
+                // If this is the first run after the app was installed via a link with an install referrer
+                // (e.g. https://play.google.com/store/apps/details?id=com.example.app&referrer=referrer.com)
+                // then the Play Store will have fired an INSTALL_REFERRER intent that this plugin handles,
+                // and `uri` will contain the referrer value ("referrer.com" in the example above).
+                // ref: https://help.tune.com/marketing-console/how-google-play-install-referrer-works/
+                
+                alert("getUri: " + uri);
+            }
+        });
+        
     } catch (e) {
         alert("fail: " + e);
     }
